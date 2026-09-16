e92c9947acc878fb43fb2742085357bcd3f00238
Prakhar Gandhi <gprakhar0@gmail.com>
2026-09-16 15:41:02 +0530

    Add automate_chain_fns for chaining function calls
    Implement automate_chain_fns to apply a chain of functions.

---
+++ chain_fn.py
@@ -0,0 +1,20 @@
+from funchain import chain
+
+# def add_two(num: int) -> int:
+#     return num + 2
+
+# def double(num: int) -> int:
+#     return num * 2
+
+
+def automate_chain_fns(list_of_str_functions, initial_state):
+    functions = [globals()[fn_name] for fn_name in list_of_str_functions]
+    fun = chain(*functions)
+    return fun(initial_state)
+
+
+# if __name__ == "__main__":
+#     list_of_str_functions = ["add_two","double","add_two","add_two","add_two"]
+#     initial_state = 5
+#     chaining_fn_output = automate_chain_fns(list_of_str_functions,initial_state)
+#     print(chaining_fn_output)
