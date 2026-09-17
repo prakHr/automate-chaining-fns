cb5e03abeaaabf06e2f8961f95515fe9c878fc13
Prakhar Gandhi <gprakhar0@gmail.com>
2026-09-17 17:06:33 +0530

    Update chain_fn.py

--- chain_fn.py
+++ chain_fn.py
@@ -10,8 +10,8 @@ from funchain import chain
 def automate_chain_fns(list_of_str_functions, initial_state):
     functions = [globals()[fn_name] for fn_name in list_of_str_functions]
     fun = chain(*functions)
-    output = fun(initial_state)
-    return output
+    final_state = fun(initial_state)
+    return final_state
 
 # if __name__ == "__main__":
 #     list_of_str_functions = ["add_two","double","add_two","add_two","add_two"]
